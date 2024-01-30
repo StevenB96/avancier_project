a325e8559871645ec0d140484b396ddb0fe7968f
StevenB96 <62147124+StevenB96@users.noreply.github.com>
2024-01-30 23:21:42 +0000

    Un-did some hard work

--- course/apps.py
+++ course/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CourseTypeConfig(AppConfig):
+class CourseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'course_type'
+    name = 'course'
---
+++ course/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+class Address(models.Model):
+    # Fields for the Address model if needed
+    pass
+
+class Party(models.Model):
+    # Fields for the Party model if needed
+    pass
+
+class Venue(models.Model):
+    # Fields for the Venue model if needed
+    pass
+
+class Attendee(models.Model):
+    # Fields for the Attendee model if needed
+    pass
+
+class Hotel(models.Model):
+    # Fields for the Hotel model if needed
+    pass
+
+class Certificate(models.Model):
+    # Fields for the Certificate model if needed
+    pass
+
+class Course(models.Model):
+    # Fields for the Course model if needed
+    pass
+
+class CourseCertificate(models.Model):
+    # Fields for the CourseCertificate model if needed
+    pass
+
+class Enquiry(models.Model):
+    # Fields for the Enquiry model if needed
+    pass
+
+class BookingInvoice(models.Model):
+    # Fields for the BookingInvoice model if needed
+    pass
+
+class CourseType(models.Model):
+    # Fields for the CourseType model if needed
+    pass
